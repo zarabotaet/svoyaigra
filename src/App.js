6570e30560fe89afd0311105954022c6370cca9e
zarabotaet <3arabotaet@gmail.com>
2022-07-13 17:14:27 +0300

    install react

---
+++ src/App.js
@@ -0,0 +1,272 @@
+export const App = () => {
+  return (
+    <div className="container">
+      <main className="m-main">
+        <div className="m-main__wrap">
+          <div className="top">
+            <ul className="top__list">
+              <li className="top__item">Цвета</li>
+              <li className="top__item">Цифры</li>
+              <li className="top__item">Люди</li>
+              <li className="top__item">Творения</li>
+              <li className="top__item">Места</li>
+              <li className="top__item">События</li>
+              <li className="top__item">Чудеса</li>
+            </ul>
+          </div>
+          <div className="questions">
+            <div className="questions__wrap">
+              <div className="questions__content content">
+                <div className="content__item pic content__item--red">
+                  <a href="#one-1" className="content__link link">
+                    1
+                  </a>
+                </div>
+                <div className="content__item pic content__item--orange">
+                  <a href="#two-1" className="content__link link">
+                    1
+                  </a>
+                </div>
+                <div className="content__item pic content__item--yellow">
+                  <a href="#three-1" className="content__link link">
+                    1
+                  </a>
+                </div>
+                <div className="content__item pic content__item--green">
+                  <a href="#four-1" className="content__link">
+                    1
+                  </a>
+                </div>
+                <div className="content__item pic content__item--lblue">
+                  <a href="#five-1" className="content__link">
+                    1
+                  </a>
+                </div>
+                <div className="content__item pic content__item--blue">
+                  <a href="#six-1" className="content__link">
+                    1
+                  </a>
+                </div>
+                <div className="content__item pic content__item--violet">
+                  <a href="#seven-1" className="content__link">
+                    1
+                  </a>
+                </div>
+                <div className="content__item pic  content__item--red">
+                  <a href="#one-2" className="content__link">
+                    2
+                  </a>
+                </div>
+                <div className="content__item pic content__item--orange">
+                  <a href="#two-2" className="content__link">
+                    2
+                  </a>
+                </div>
+                <div className="content__item pic content__item--yellow">
+                  <a href="#three-2" className="content__link">
+                    2
+                  </a>
+                </div>
+                <div className="content__item pic content__item--green">
+                  <a href="#four-2" className="content__link">
+                    2
+                  </a>
+                </div>
+                <div className="content__item pic content__item--lblue">
+                  <a href="#five-2" className="content__link">
+                    2
+                  </a>
+                </div>
+                <div className="content__item pic content__item--blue">
+                  <a href="#six-2" className="content__link">
+                    2
+                  </a>
+                </div>
+                <div className="content__item pic content__item--violet">
+                  <a href="#seven-2" className="content__link">
+                    2
+                  </a>
+                </div>
+                <div className="content__item pic content__item--red">
+                  <a href="#one-3" className="content__link">
+                    3
+                  </a>
+                </div>
+                <div className="content__item pic  content__item--orange">
+                  <a href="#two-3" className="content__link">
+                    3
+                  </a>
+                </div>
+                <div className="content__item pic content__item--yellow">
+                  <a href="#three-3" className="content__link">
+                    3
+                  </a>
+                </div>
+                <div className="content__item pic content__item--green">
+                  <a href="#four-3" className="content__link">
+                    3
+                  </a>
+                </div>
+                <div className="content__item pic content__item--lblue">
+                  <a href="#five-3" className="content__link">
+                    3
+                  </a>
+                </div>
+                <div className="content__item pic content__item--blue">
+                  <a href="#six-3" className="content__link">
+                    3
+                  </a>
+                </div>
+                <div className="content__item pic content__item--violet">
+                  <a href="#seven-3" className="content__link">
+                    3
+                  </a>
+                </div>
+                <div className="content__item pic content__item--red">
+                  <a href="#one-4" className="content__link">
+                    4
+                  </a>
+                </div>
+                <div className="content__item pic content__item--orange">
+                  <a href="#two-4" className="content__link">
+                    4
+                  </a>
+                </div>
+                <div className="content__item pic content__item--yellow">
+                  <a href="#three-4" className="content__link">
+                    4
+                  </a>
+                </div>
+                <div className="content__item pic content__item--green">
+                  <a href="#four-4" className="content__link">
+                    4
+                  </a>
+                </div>
+                <div className="content__item pic content__item--lblue">
+                  <a href="#five-4" className="content__link">
+                    4
+                  </a>
+                </div>
+                <div className="content__item pic content__item--blue">
+                  <a href="#six-4" className="content__link">
+                    4
+                  </a>
+                </div>
+                <div className="content__item pic content__item--violet">
+                  <a href="#seven-4" className="content__link">
+                    4
+                  </a>
+                </div>
+                <div className="content__item pic  content__item--red">
+                  <a href="#one-5" className="content__link">
+                    5
+                  </a>
+                </div>
+                <div className="content__item pic content__item--orange">
+                  <a href="#two-5" className="content__link">
+                    5
+                  </a>
+                </div>
+                <div className="content__item pic content__item--yellow">
+                  <a href="#three-5" className="content__link">
+                    5
+                  </a>
+                </div>
+                <div className="content__item pic content__item--green">
+                  <a href="#four-5" className="content__link">
+                    5
+                  </a>
+                </div>
+                <div className="content__item pic content__item--lblue">
+                  <a href="#five-5" className="content__link">
+                    5
+                  </a>
+                </div>
+                <div className="content__item pic content__item--blue">
+                  <a href="#six-5" className="content__link">
+                    5
+                  </a>
+                </div>
+                <div className="content__item pic content__item--violet">
+                  <a href="#seven-5" className="content__link">
+                    5
+                  </a>
+                </div>
+                <div className="content__item pic content__item--red">
+                  <a href="#one-6" className="content__link">
+                    6
+                  </a>
+                </div>
+                <div className="content__item pic content__item--orange">
+                  <a href="#two-6" className="content__link">
+                    6
+                  </a>
+                </div>
+                <div className="content__item pic  content__item--yellow">
+                  <a href="#three-6" className="content__link">
+                    6
+                  </a>
+                </div>
+                <div className="content__item pic content__item--green">
+                  <a href="#four-6" className="content__link">
+                    6
+                  </a>
+                </div>
+                <div className="content__item pic content__item--lblue">
+                  <a href="#five-6" className="content__link">
+                    6
+                  </a>
+                </div>
+                <div className="content__item pic  content__item--blue">
+                  <a href="#six-6" className="content__link">
+                    6
+                  </a>
+                </div>
+                <div className="content__item pic content__item--violet">
+                  <a href="#seven-6" className="content__link">
+                    6
+                  </a>
+                </div>
+                <div className="content__item pic content__item--red">
+                  <a href="#one-7" className="content__link">
+                    7
+                  </a>
+                </div>
+                <div className="content__item pic content__item--orange">
+                  <a href="#two-7" className="content__link">
+                    7
+                  </a>
+                </div>
+                <div className="content__item pic content__item--yellow">
+                  <a href="#three-7" className="content__link">
+                    7
+                  </a>
+                </div>
+                <div className="content__item pic content__item--green">
+                  <a href="#four-7" className="content__link">
+                    7
+                  </a>
+                </div>
+                <div className="content__item pic content__item--lblue">
+                  <a href="#five-7" className="content__link">
+                    7
+                  </a>
+                </div>
+                <div className="content__item pic content__item--blue">
+                  <a href="#six-7" className="content__link">
+                    7
+                  </a>
+                </div>
+                <div className="content__item pic content__item--violet">
+                  <a href="#seven-7" className="content__link">
+                    7
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
